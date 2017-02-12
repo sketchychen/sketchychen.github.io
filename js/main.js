06270e710bf299978d44ff7d33041f6579f747e4
skylocke <mathematical.fiction@gmail.com>
2017-02-12 13:33:27 -0800

    lol

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 $(document).ready(function () {
+  var navTop = $('#nav').offset().top;
+  var sections = []
+  $("section").each(function() {
+    // console.log($(this).attr('id'), $(this).offset().top)
+    sections.push({id: "#" + $(this).attr('id'), top: $(this).offset().top})
+  });
+
+  console.log(sections);
 
     $(window).bind('scroll', function () {
-        var vPos = $(window).scrollTop();
-        var totalH = $('#nav').offset().top;
-        var finalSize = totalH - vPos;
+        var windowTop = $(window).scrollTop();
+        var navWindowTop = navTop - windowTop;
+        // console.log(navWindowTop);
+        // console.log(projectsTop-windowTop);
 
-        // console.log(finalSize);
-
-        if (finalSize <= 0) {
+        if (navWindowTop <= 0) {
             $('.navbar').addClass('navbar-docked');
         } else {
             $('.navbar').removeClass('navbar-docked');
         }
+
+        if (windowTop >= sections[0].top && windowTop < sections[1].top) {}
+
     });
 
     $(".navbar-button").each(function() {
@@ -24,7 +34,7 @@ $(document).ready(function () {
     })
 
     function scrollTo(elementID) {
-      console.log(elementID);
+      // console.log(elementID);
         $('html, body').animate({
             scrollTop: $(elementID).offset().top
         }, 1000);
